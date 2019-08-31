ef868d3f2fdff81f43b09f2c934570234b10e0dd
Haron Arama <arama006@umn.edu>
2019-08-30 18:01:37 -0700

    Work on validation and sign up form

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,10 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
-import { UserData } from '../../providers/user-data';
-
-import { UserOptions } from '../../utils/interfaces/user-options';
+import { UserRegistrationOptions } from '../../utils/interfaces/user-options';
+import {AuthService} from '../../utils/services/auth.service';
+import {IonContent, IonSlides, NavController} from '@ionic/angular';
+import {State} from '../../utils/interfaces/locations/state';
+import {STATES} from '../../utils/mocks/states.mock';
 
 
 
@@ -14,20 +15,115 @@ import { UserOptions } from '../../utils/interfaces/user-options';
   styleUrls: ['./signup.scss'],
 })
 export class SignupPage {
-  signup: UserOptions = { username: '', password: '' };
+
+  @ViewChild(IonSlides, {static: false}) slides: IonSlides;
+  @ViewChild(IonContent, {static: false}) content: IonContent;
+
+  signup: UserRegistrationOptions = {
+    first_name: undefined,
+    last_name: undefined,
+    username: undefined,
+    email: undefined,
+    phone: undefined,
+    birthday: undefined,
+    password: undefined,
+    confirm_password: undefined,
+    freelancer: false,
+    city_id: undefined,
+    street_address: undefined,
+    city: undefined,
+    state: undefined,
+    zip: undefined
+  };
   submitted = false;
+  maxYear = (new Date()).getFullYear() - 12;
+  states: State[] = STATES;
+  progress = 0;
+  pageTitle = 'Sign Up';
 
   constructor(
-    public router: Router,
-    public userData: UserData
-  ) {}
+    private authService: AuthService,
+    public navCtrl: NavController
+  ) { }
 
   onSignup(form: NgForm) {
     this.submitted = true;
 
     if (form.valid) {
-      // this.userData.signup(this.signup.username);
-      this.router.navigateByUrl('/app/tabs/schedule');
+      this.authService.register(this.signup)
+        .subscribe(() => this.navCtrl.navigateRoot('/app/tabs/marketplace'));
+    }
+  }
+
+  onSlideChange() {
+    this.slides.getActiveIndex()
+      .then((index) => {
+        switch (index) {
+          case 0:
+            this.pageTitle = 'Sign Up';
+            this.content.scrollToTop(500);
+            break;
+          case 1:
+            this.pageTitle = 'Security';
+            this.content.scrollToTop(500);
+            break;
+          case 2:
+            this.pageTitle = 'Location';
+            this.content.scrollToTop(500);
+            break;
+        }
+      });
+  }
+
+  setProgress() {
+    this.progress = 0;
+
+    if (this.signup.first_name) {
+      this.progress = 1 / 12;
+    }
+
+    if (this.signup.last_name) {
+      this.progress = 2 / 12;
+    }
+
+    if (this.signup.username) {
+      this.progress = 3 / 12;
+    }
+
+    if (this.signup.email) {
+      this.progress = 4 / 12;
+    }
+
+    if (this.signup.phone) {
+      this.progress = 5 / 12;
+    }
+
+    if (this.signup.birthday) {
+      this.progress = 6 / 12;
+    }
+
+    if (this.signup.password) {
+      this.progress = 7 / 12;
+    }
+
+    if (this.signup.confirm_password) {
+      this.progress = 8 / 12;
+    }
+
+    if (this.signup.street_address) {
+      this.progress = 9 / 12;
+    }
+
+    if (this.signup.city) {
+      this.progress = 10 / 12;
+    }
+
+    if (this.signup.state) {
+      this.progress = 11 / 12;
+    }
+
+    if (this.signup.zip) {
+      this.progress = 12 / 12;
     }
   }
 }
