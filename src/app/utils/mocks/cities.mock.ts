849da7f19f3d076a896f2998dd114998f0b7287b
Solomon04 <Antoinesolomon5@gmail.com>
2019-10-13 21:53:24 -0500

    Assets update

--- src/app/utils/mocks/cities.mock.ts
+++ src/app/utils/mocks/cities.mock.ts
@@ -8,34 +8,34 @@ export const CITIES: City[] = [
     name: 'Rochester',
     state: STATES[0],
     description: 'Rochester is home to the largest and most well renowned hospital in the world. It is also home to FAVR and you\'ll find no shortage of help on tasks here.',
-    src: 'http://localhost:8100/assets/img/rochester.jpg'
+    src: 'assets/img/rochester.jpg'
   },
   {
     id: 2,
     name: 'Minneapolis',
     state: STATES[0],
     description: 'Beautiful Minneapolis is one of America\'s fastest growing cities. Let FAVR help you take part in that growth.',
-    src: 'http://localhost:8100/assets/img/minneapolis.jpg'
+    src: 'assets/img/minneapolis.jpg'
   },
   {
     id: 3,
     name: 'Winona',
     state: STATES[0],
     description: 'Picturesque Winona on the bank of the Mississippi River is a premiere college town home to Winona State University and St. Mary\'s University. Let FAVR help you find help on tasks here.',
-    src: 'http://localhost:8100/assets/img/winona.jpg'
+    src: 'assets/img/winona.jpg'
   },
   {
     id: 4,
     name: 'Brookings',
     state: STATES[2],
     description: 'A regular city in middle America home to the popular South Dakota State University. Let FAVR help you find help on tasks here.',
-    src: 'http://localhost:8100/assets/img/brookings.jpg'
+    src: 'assets/img/brookings.jpg'
   },
   {
     id: 5,
     name: 'Mankato',
     state: STATES[0],
     description: 'A bustling small town in Minnesota home to the great Mankato State University. Let FAVR help you find help tasks on tasks here.',
-    src: 'http://localhost:8100/assets/img/mankato.jpg'
+    src: 'assets/img/mankato.jpg'
   },
 ];
